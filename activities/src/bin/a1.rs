d445bfbf89bf35be48d4d7d35e4cd60e676a9007
Iwan Imsand <66054653+iwanimsand@users.noreply.github.com>
2023-11-04 16:21:05 +0000

    finished a1

--- activities/src/bin/a1.rs
+++ activities/src/bin/a1.rs
@@ -8,4 +8,20 @@
 // * Use a function to display your last name
 // * Use the println macro to display messages to the terminal
 
-fn main() {}
+fn first_name() {
+    println!("Iwan");
+}
+
+fn last_name() {
+    println!("Imsand");
+}
+
+fn main() {
+    first_name();
+    last_name();
+}
+
+// run the program with:
+// `cargo run --bin a1`
+// or
+// `cargo run -q --bin a1`
