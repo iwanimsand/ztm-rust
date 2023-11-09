b17305762a58fe111353a460198cfd94409c6b10
Iwan Imsand <66054653+iwanimsand@users.noreply.github.com>
2023-11-09 14:22:00 +0000

    finished a3b

--- activities/src/bin/a3b.rs
+++ activities/src/bin/a3b.rs
@@ -9,4 +9,13 @@
 // * Use an if..else if..else block to determine which message to display
 // * Use the println macro to display messages to the terminal
 
-fn main() {}
+fn main() {
+    let my_number: i8 = 10;
+    if my_number > 5 {
+        println!(">{my_number}");
+    } else if my_number < 5 {
+        println!("<5");
+    } else {
+        println!("=5")
+    }
+}
