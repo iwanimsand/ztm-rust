57450ac0b627129e0927756aedb4c4108fae8ac6
Iwan Imsand <66054653+iwanimsand@users.noreply.github.com>
2023-11-09 14:23:05 +0000

    fix a3b

--- activities/src/bin/a3b.rs
+++ activities/src/bin/a3b.rs
@@ -12,10 +12,10 @@
 fn main() {
     let my_number: i8 = 10;
     if my_number > 5 {
-        println!(">{my_number}");
+        println!(">5");
     } else if my_number < 5 {
         println!("<5");
     } else {
-        println!("=5")
+        println!("=5");
     }
 }
