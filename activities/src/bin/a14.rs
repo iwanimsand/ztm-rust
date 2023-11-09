cd9b138ccbc3d969f994d6102b8de80229eedef7
Iwan Imsand <66054653+iwanimsand@users.noreply.github.com>
2023-11-09 18:57:19 +0000

    finished a14

--- activities/src/bin/a14.rs
+++ activities/src/bin/a14.rs
@@ -11,4 +11,44 @@
 // * Use an if expression to determine which person's info should be printed
 // * The name and colors should be printed using a function
 
-fn main() {}
+struct Person {
+    age: i32,
+    name: String,
+    fav_color: String,
+}
+
+fn print(data: &str) {
+    println!("{:?}", data);
+}
+
+impl Person {
+    fn new(age: i32, name: String, fav_color: String) -> Self {
+        Self {
+            age,
+            name,
+            fav_color,
+        }
+    }
+
+    fn print(&self) {
+        print(&self.name);
+        print(&self.fav_color);
+    }
+}
+
+fn main() {
+    let people = vec![
+        Person::new(5, String::from("A"), String::from("red")),
+        Person::new(11, String::from("B"), String::from("green")),
+        Person::new(10, String::from("C"), String::from("blue")),
+        Person::new(20, String::from("D"), String::from("yellow")),
+        Person::new(3, String::from("E"), String::from("orange")),
+        Person::new(4, String::from("F"), String::from("black")),
+    ];
+
+    for person in people {
+        if person.age <= 10 {
+            person.print();
+        }
+    }
+}
