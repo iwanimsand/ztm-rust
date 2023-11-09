bb9e5829d38ac2460f674f957c7ff6ab1474a846
Iwan Imsand <66054653+iwanimsand@users.noreply.github.com>
2023-11-09 16:31:46 +0000

    finished a8

--- activities/src/bin/a8.rs
+++ activities/src/bin/a8.rs
@@ -9,4 +9,44 @@
 // * Use a function to print out the drink flavor and ounces
 // * Use a match expression to print the drink flavor
 
-fn main() {}
+#[derive(Debug)]
+enum Flavor {
+    Sparkling,
+    Sweet,
+    Fruity,
+}
+
+struct Drink {
+    flavor: Flavor,
+    fluid_oz: f64,
+}
+
+fn print_drink(drink: Drink) {
+    match drink.flavor {
+        Flavor::Sparkling => println!("sparkling"),
+        Flavor::Sweet => println!("sweet"),
+        Flavor::Fruity => println!("fruity"),
+    }
+
+    println!("oz: {:?}", drink.fluid_oz);
+}
+
+fn main() {
+    let sweet = Drink {
+        flavor: Flavor::Sweet,
+        fluid_oz: 6.0,
+    };
+    print_drink(sweet);
+
+    let fruity = Drink {
+        flavor: Flavor::Fruity,
+        fluid_oz: 10.0,
+    };
+    print_drink(fruity);
+
+    let sparkling = Drink {
+        flavor: Flavor::Sparkling,
+        fluid_oz: 3.0,
+    };
+    print_drink(sparkling);
+}
