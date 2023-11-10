cc9cbd12eae08c2aa85fc7ca3f4c1bb5a3bc5d8d
Iwan Imsand <66054653+iwanimsand@users.noreply.github.com>
2023-11-10 17:30:11 +0000

    finished a22

--- activities/src/bin/a22.rs
+++ activities/src/bin/a22.rs
@@ -25,13 +25,14 @@ fn clamp(n: i32, lower: i32, upper: i32) -> i32 {
 fn div(a: i32, b: i32) -> Option<i32> {
     if b == 0 {
         return None;
+    } else {
+        Some(a / b)
     }
-    Some(a / b)
 }
 
 /// Takes two strings and places them immediately one after another.
 fn concat(first: &str, second: &str) -> String {
-    format!("{} {}", first, second)
+    format!("{}{}", first, second)
 }
 
 fn main() {}
@@ -46,16 +47,25 @@ mod test {
         assert_eq!(clamp(5, 10, 20), 10);
         assert_eq!(clamp(25, 10, 20), 20);
         assert_eq!(clamp(15, 10, 20), 15);
+        assert_eq!(clamp(10, 10, 20), 10);
+
+        let result = clamp(10, 100, 1000);
+        let expected = 100;
+        assert_eq!(result, expected, "should be 100");
     }
 
     #[test]
     fn test_div() {
         assert_eq!(div(10, 2).unwrap(), 5);
         assert_eq!(div(10, 0), None);
+
+        let result = div(1, 1);
+        let expected = Some(1);
+        assert_eq!(result, expected);
     }
 
     #[test]
     fn test_concat() {
-        assert_eq!(concat("1", "2"), "1 2");
+        assert_eq!(concat("1", "2"), "12");
     }
 }
