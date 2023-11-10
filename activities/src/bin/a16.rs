59de84ebae07a280fa5c5b464f9aa33768088e38
Iwan Imsand <66054653+iwanimsand@users.noreply.github.com>
2023-11-10 13:09:56 +0000

    finished a16

--- activities/src/bin/a16.rs
+++ activities/src/bin/a16.rs
@@ -8,4 +8,26 @@
 // * Use a struct containing the student's name and locker assignment
 // * The locker assignment should use an Option<i32>
 
-fn main() {}
+struct Student {
+    name: String,
+    locker: Option<i32>,
+}
+
+impl Student {
+    fn new(name: String, locker: Option<i32>) -> Self {
+        return Self { name, locker };
+    }
+
+    fn print(&self) {
+        match self.locker {
+            Some(num) => println!("locker number: {:?}", num),
+            None => println!("no locker assigend"),
+        }
+    }
+}
+
+fn main() {
+    let mary = Student::new("Mary".to_owned(), Some(3));
+
+    mary.print();
+}
