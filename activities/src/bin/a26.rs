888388ff3c41aba05b4d270767984db16fd0a6cb
Iwan Imsand <66054653+iwanimsand@users.noreply.github.com>
2023-11-11 10:34:43 +0100

    finished a26

--- activities/src/bin/a26.rs
+++ activities/src/bin/a26.rs
@@ -8,4 +8,12 @@
 // * (OPTIONAL) Read the documentation section `Formatting and Parsing`
 //   for examples on how to create custom time formats
 
-fn main() {}
+fn main() {
+    use chrono::prelude::*;
+
+    let local: DateTime<Local> = Local::now();
+    let utc: DateTime<Utc> = Utc::now();
+
+    println!("Local: {:?}", local.format("%Y-%m-%d %H:%M:%S").to_string());
+    println!("UTC: {:?}", utc);
+}
