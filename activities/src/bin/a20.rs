7d9362b17a6d646893c19949a78c016c92a72028
Iwan Imsand <66054653+iwanimsand@users.noreply.github.com>
2023-11-12 20:05:39 +0100

    own solution for a20

--- activities/src/bin/a20.rs
+++ activities/src/bin/a20.rs
@@ -23,4 +23,65 @@
 // * The program should be case-insensitive (the user should be able to type
 //   Reboot, reboot, REBOOT, etc.)
 
-fn main() {}
+use std::io;
+
+#[derive(PartialEq, Debug)]
+enum PowerState {
+    Off,
+    Sleep,
+    Reboot,
+    Shutdown,
+    Hibernate,
+}
+
+impl PowerState {
+    fn from_string(text: &str) -> Option<PowerState> {
+        match text.to_lowercase().trim() {
+            "off" => Some(PowerState::Off),
+            "sleep" => Some(PowerState::Sleep),
+            "reboot" => Some(PowerState::Reboot),
+            "shutdown" => Some(PowerState::Shutdown),
+            "hibernate" => Some(PowerState::Hibernate),
+            _ => return None,
+        }
+    }
+
+    fn execute(power_state: PowerState) {
+        match power_state {
+            PowerState::Off => println!("turning off"),
+            PowerState::Sleep => println!("entering sleep mode"),
+            PowerState::Reboot => println!("initiating reboot"),
+            PowerState::Shutdown => println!("shutting down"),
+            PowerState::Hibernate => println!("going into hibernate mode"),
+        }
+    }
+}
+
+fn main() {
+    println!("Enter a state:");
+
+    let mut buffer = String::new();
+    let result = io::stdin().read_line(&mut buffer);
+    match result {
+        Ok(_) => {
+            let power_state = PowerState::from_string(&buffer);
+            match power_state {
+                Some(power_state) => PowerState::execute(power_state),
+                None => println!("I'm not familiar with that state"),
+            }
+        }
+        Err(e) => println!("{:?}", e),
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use crate::PowerState;
+
+    #[test]
+    fn test_from_string() {
+        let result = PowerState::from_string("OFF");
+        let expected = Some(PowerState::Off);
+        assert_eq!(expected, result);
+    }
+}
