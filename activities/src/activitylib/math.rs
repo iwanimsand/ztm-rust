065718816570705f31205f3a3465b47e1481ea4a
Iwan Imsand <66054653+iwanimsand@users.noreply.github.com>
2023-11-12 11:14:44 +0100

    own solution and experimentation with a26c

---
+++ activities/src/activitylib/math.rs
@@ -0,0 +1,8 @@
+pub mod arithmetic;
+
+pub mod factorial {
+
+    pub fn mul(lhs: isize, rhs: isize) -> isize {
+        lhs * rhs
+    }
+}
---
+++ activities/src/activitylib/math/arithmetic.rs
@@ -0,0 +1,7 @@
+pub fn add(lhs: isize, rhs: isize) -> isize {
+    lhs + rhs
+}
+
+pub fn sub(lhs: isize, rhs: isize) -> isize {
+    lhs - rhs
+}
