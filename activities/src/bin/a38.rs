db154d5450e9f075181c374706d71f9b0e3a0ed1
Iwan Imsand <66054653+iwanimsand@users.noreply.github.com>
2024-03-03 14:00:47 +0100

    finished a38

--- activities/src/bin/a38.rs
+++ activities/src/bin/a38.rs
@@ -8,6 +8,8 @@
 // Notes:
 // * Use the join function to wait for threads to finish
 
+use std::thread;
+
 fn msg_hello() -> &'static str {
     use std::time::Duration;
     std::thread::sleep(Duration::from_millis(1000));
@@ -26,4 +28,14 @@ fn msg_excited() -> &'static str {
     "!"
 }
 
-fn main() {}
+fn main() {
+    let msg_one = thread::spawn(move || msg_hello());
+    let msg_two = thread::spawn(move || msg_thread());
+    let msg_three = thread::spawn(move || msg_excited());
+
+    let msg_one = msg_one.join().expect("failed to join msg one");
+    let msg_two = msg_two.join().expect("failed to join msg two");
+    let msg_three = msg_three.join().expect("failed to join msg three");
+
+    println!("{}{}{}", msg_one, msg_two, msg_three);
+}
